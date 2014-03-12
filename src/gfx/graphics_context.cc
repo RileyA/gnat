40d191efa72552838b7a2f5ad5b27993d6c5c932
Riley Adams <rileya@cs.washington.edu>
2014-03-12 11:02:27 -0700

    Add ---- separators and set GL version to 2.0 for now.

--- src/gfx/graphics_context.cc
+++ src/gfx/graphics_context.cc
@@ -8,18 +8,22 @@ namespace gnat {
 GraphicsContext::GraphicsContext(GLPlatformContext* platform_context)
   : platform_context_(platform_context) {
 }
+//---------------------------------------------------------------------------
 
 GraphicsContext::~GraphicsContext() {
   
 }
+//---------------------------------------------------------------------------
 
 void GraphicsContext::Update(Real delta) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
   platform_context_->UpdateDisplay();
 }
+//---------------------------------------------------------------------------
 
 void GraphicsContext::SetClearColor(ColorF32 color) {
   glClearColor(color.r, color.g, color.b, color.a);
 }
+//---------------------------------------------------------------------------
 
 }  // namespace gnat
--- src/platform/sdl/sdl_platform_context.cc
+++ src/platform/sdl/sdl_platform_context.cc
@@ -3,89 +3,106 @@
 
 namespace gnat {
 
-  SDLPlatformContext::SDLPlatformContext(bool gl, bool input)
-    : gl_context_(gl),
-      input_context_(input) {
-    uint32_t flags = 0;
-    if (gl_context_)
-      flags |= SDL_INIT_VIDEO;
-    if (input_context_)
-      flags |= SDL_INIT_JOYSTICK;
+#define SDL_ERROR() check_error(__LINE__, __FILE__)
+#define SDL_CHECK_ERROR() DCHECK(!check_error(__LINE__, __FILE__))
+bool check_error(int line, const char* filename) {
+#ifndef NDEBUG
+	const char* error = SDL_GetError();
+	if (*error != '\0') {
+		printf("SDL Error: %s %s:%d\n", error, filename, line);
+		SDL_ClearError();
+    return true;
+	}
+  return false;
+#endif
+}
 
-    // If we're not being used at all, why did we get instantiated in the first
-    // place?
-    DCHECK(flags);
-    
-    if (SDL_Init(flags) < 0)
-      exit(1);
+SDLPlatformContext::SDLPlatformContext(bool gl, bool input)
+  : gl_context_(gl),
+    input_context_(input) {
+  uint32_t flags = 0;
+  if (gl_context_)
+    flags |= SDL_INIT_VIDEO;
+  if (input_context_)
+    flags |= SDL_INIT_JOYSTICK;
+
+  // If we're not being used at all, why did we get instantiated in the first
+  // place?
+  DCHECK(flags);
+  
+  if (SDL_Init(flags) < 0)
+    exit(1);
+}
+
+SDLPlatformContext::~SDLPlatformContext() {}
+
+void SDLPlatformContext::InitDisplay(size_t w, size_t h, bool fullscreen) {
+  DCHECK(gl_context_);
+
+  // TODO: GL version handling stuffs
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  window_ = SDL_CreateWindow(APPLICATION_NAME, SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED,w, h,
+                                (fullscreen ? SDL_WINDOW_FULLSCREEN : 0) 
+                                | SDL_WINDOW_OPENGL);
+  SDL_ERROR();
+  if (!window_) {
+    // TODO: settle on a consistent error handling scheme for cases like this
+    exit(1);
   }
 
-  SDLPlatformContext::~SDLPlatformContext() {}
+  context_ = SDL_GL_CreateContext(window_);
+  SDL_CHECK_ERROR();
 
-  void SDLPlatformContext::InitDisplay(size_t w, size_t h, bool fullscreen) {
-    DCHECK(gl_context_);
+  CreateSignal("window_closed");
+  CreateSignal("window_resized");
+}
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    window_ = SDL_CreateWindow(APPLICATION_NAME, SDL_WINDOWPOS_CENTERED,
-                                  SDL_WINDOWPOS_CENTERED,w, h,
-                                  (fullscreen ? SDL_WINDOW_FULLSCREEN : 0) 
-                                  | SDL_WINDOW_OPENGL);
-    if (!window_) {
-      // TODO: settle on a consistent error handling scheme for cases like this
-      exit(1);
+void SDLPlatformContext::UpdateDisplay() {
+  HandleEvents();
+  SwapBuffers();
+}
+
+void SDLPlatformContext::SwapBuffers() {
+  SDL_GL_SwapWindow(window_);
+}
+
+long SDLPlatformContext::GetWindowHandle() {
+  NOTREACHED();
+  return 0;
+}
+
+void SDLPlatformContext::InitInput() {
+}
+
+void SDLPlatformContext::UpdateInput() {
+  HandleEvents();
+}
+
+bool SDLPlatformContext::IsKeyPressed() {
+  NOTREACHED();
+  return false;
+}
+
+void SDLPlatformContext::Deinit() {
+  if (gl_context_) {
+    SDL_GL_DeleteContext(context_);
+    SDL_DestroyWindow(window_);
+  }
+  SDL_Quit();
+}
+
+void SDLPlatformContext::HandleEvents() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        GetSignal("window_closed")->Fire(NULL);
+        break;
     }
-
-    context_ = SDL_GL_CreateContext(window_);
-
-    CreateSignal("window_closed");
-    CreateSignal("window_resized");
-  }
-
-  void SDLPlatformContext::UpdateDisplay() {
-    HandleEvents();
-    SwapBuffers();
-  }
-
-  void SDLPlatformContext::SwapBuffers() {
-    SDL_GL_SwapWindow(window_);
-  }
-
-  long SDLPlatformContext::GetWindowHandle() {
-    NOTREACHED();
-    return 0;
-  }
-
-  void SDLPlatformContext::InitInput() {
-  }
-
-  void SDLPlatformContext::UpdateInput() {
-    HandleEvents();
-  }
-
-  bool SDLPlatformContext::IsKeyPressed() {
-    NOTREACHED();
-    return false;
-  }
-
-  void SDLPlatformContext::Deinit() {
-    if (gl_context_) {
-      SDL_GL_DeleteContext(context_);
-      SDL_DestroyWindow(window_);
-    }
-    SDL_Quit();
-  }
-
-  void SDLPlatformContext::HandleEvents() {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-          GetSignal("window_closed")->Fire(NULL);
-          break;
-      }
-    };
   }
 }
+
+}  // namespace gnat
