9c88e3e29d74ddda5779878f889cc2bf5cdaf401
Riley Adams <rileya@cs.washington.edu>
2014-03-16 11:56:43 -0700

    Add MeshData util class.

---
+++ src/gfx/util/mesh_data.h
@@ -0,0 +1,48 @@
+#ifndef gnat_gfx_util_mesh_data_H
+#define gnat_gfx_util_mesh_data_H
+
+#include "gnat.h"
+#include "util/color.h"
+
+namespace gnat {
+
+/** A class containing raw mesh data (usually we'll want to keep mesh
+ *  data in GPU memory, so avoid using this in realtime cases). */
+class MeshData {
+ public:
+
+  MeshData();
+  ~MeshData();
+
+  void AddPosition(Vector3 p);
+  void AddNormal(Vector3 n);
+  void AddDiffuse(ColorF32);
+
+  void AddTexcoordSet();
+  void AddTexcoord(uint32_t index, float u, float v);
+  void AddTexcoord(uint32_t index, float u, float v, float w);
+
+  void AddIndex(uint32_t index);
+  void AddTriangle(uint32_t v1, uint32_t v2, uint32_t v3);
+
+  float* positions() { return &positions_[0]; }
+  float* normals() { return &normals_[0]; }
+  float* diffuse() { return &diffuse_[0]; }
+  float* texcoords() { return &diffuse_[0]; }
+
+ private:
+  
+  Vector<float> positions_;
+  Vector<float> normals_;
+  Vector<float> diffuse_;
+  Vector<Vector<float> > texcoords_;
+
+  // TODO: consider using 16-bit indices until the size exceeds 2^16 - 1.
+  // TODO add field for index scheme (strip/fan/etc)
+  Vector<uint32_t> indices_;
+
+};
+
+}  // namespace gnat
+
+#endif  // gnat_gfx_util_mesh_data_H
