8dba6577872c579db34fc11143083dfdb7ea3cce
Riley Adams <rileya@cs.washington.edu>
2014-03-16 11:56:06 -0700

    Add GL BufferObject wrapper.

---
+++ src/gfx/util/buffer_object.h
@@ -0,0 +1,55 @@
+#ifndef gnat_gfx_util_buffer_object_H 
+#define gnat_gfx_util_buffer_object_H
+
+#include "gnat.h"
+
+#include "GL/glew.h"
+#include "gl.h"
+
+namespace gnat {
+
+/** A simple wrapper over a GL buffer object */
+template <GLenum BUFFER_TYPE> 
+class BufferObject {
+public:
+	
+	BufferObject();
+	BufferObject(size_t size, GLuint usage) ;
+  ~BufferObject();
+
+	bool operator < (const BufferObject<BUFFER_TYPE>& other) const {
+		return size() < other.getSize();
+	}
+
+	inline size_t size() const { return size_; }
+	void Bind();
+	void Unbind();
+	bool IsValid() const { return handle_ != 0; }
+
+	void* MapBuffer();
+	void* GetMappedBuffer();
+	void UnmapBuffer();
+
+	void Destroy();
+
+private:
+
+	size_t size_;
+	GLuint handle_;
+	bool is_mapped_;
+	bool is_bound_;
+	// The mapped buffer, if mapped
+	void* buffer_;
+
+};
+
+// Magic typedefs to hint to the compiler that we'll use these specializations,
+// we'll hopefully get linker errors if we try to use anything else.
+typedef BufferObject<GL_PIXEL_UNPACK_BUFFER> PixelBufferObject;
+typedef BufferObject<GL_PIXEL_PACK_BUFFER> PixelBufferObjectPack;
+typedef BufferObject<GL_ARRAY_BUFFER> VertexBufferObject;
+typedef BufferObject<GL_ELEMENT_ARRAY_BUFFER> IndexBufferObject;
+
+}  // namespace gnat
+
+#endif  // gnat_gfx_util_buffer_object_H
