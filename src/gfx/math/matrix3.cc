162cdc56f087a1dcd5c53b9cc6e8f0775a447668
Riley Adams <rileya@cs.washington.edu>
2014-03-22 13:44:11 -0700

    Style cleanup for Matrix3.

--- src/gfx/math/matrix3.cc
+++ src/gfx/math/matrix3.cc
@@ -8,130 +8,117 @@ const Matrix3 Matrix3::ZERO(0,0,0,0,0,0,0,0,0);
 const Matrix3 Matrix3::IDENTITY(1,0,0,0,1,0,0,0,1);
 
 //-----------------------------------------------------------------------
-void Matrix3::SetColumn(size_t iCol, const Vector3& vec)
-{
-  //assert( 0 <= iCol && iCol < 3 );
-  m[0][iCol] = vec.x;
-  m[1][iCol] = vec.y;
-  m[2][iCol] = vec.z;
+void Matrix3::SetColumn(size_t col, const Vector3& vec) {
+  //assert( 0 <= col && col < 3 );
+  m[0][col] = vec.x;
+  m[1][col] = vec.y;
+  m[2][col] = vec.z;
 }
-
 //-----------------------------------------------------------------------
-void Matrix3::FromAxes(const Vector3& xAxis, const Vector3& yAxis, const Vector3& zAxis)
-{
+
+void Matrix3::FromAxes(const Vector3& xAxis, const Vector3& yAxis, const Vector3& zAxis) {
   SetColumn(0,xAxis);
   SetColumn(1,yAxis);
   SetColumn(2,zAxis);
 }
 
 //-----------------------------------------------------------------------
-Vector3 Matrix3::operator* (const Vector3& rkPoint) const
-{
-  Vector3 kProd(0,0,0);
-  for (size_t iRow = 0; iRow < 3; iRow++)
-  {
-    kProd[iRow] =
-      m[iRow][0]*rkPoint[0] +
-      m[iRow][1]*rkPoint[1] +
-      m[iRow][2]*rkPoint[2];
-  }
-  return kProd;
-}
 
-//-----------------------------------------------------------------------
-Vector3 operator* (const Vector3& rkPoint, const Matrix3& rkMatrix)
-{
-  Vector3 kProd(0,0,0);
-  for (size_t iRow = 0; iRow < 3; iRow++)
-  {
-    kProd[iRow] =
-      rkPoint[0]*rkMatrix.m[0][iRow] +
-      rkPoint[1]*rkMatrix.m[1][iRow] +
-      rkPoint[2]*rkMatrix.m[2][iRow];
+Vector3 Matrix3::operator* (const Vector3& point) const {
+  Vector3 prod(0,0,0);
+  for (size_t row = 0; row < 3; row++) {
+    prod[row] =
+      m[row][0]*point[0] +
+      m[row][1]*point[1] +
+      m[row][2]*point[2];
   }
-  return kProd;
-}
-
-//-----------------------------------------------------------------------
-Matrix3 Matrix3::operator* (Real fScalar) const
-{
-  Matrix3 kProd;
-  for (size_t iRow = 0; iRow < 3; iRow++)
-  {
-    for (size_t iCol = 0; iCol < 3; iCol++)
-      kProd[iRow][iCol] = fScalar*m[iRow][iCol];
-  }
-  return kProd;
+  return prod;
 }
 //-----------------------------------------------------------------------
-Matrix3 operator* (Real fScalar, const Matrix3& rkMatrix)
-{
-  Matrix3 kProd;
-  for (size_t iRow = 0; iRow < 3; iRow++)
-  {
-    for (size_t iCol = 0; iCol < 3; iCol++)
-      kProd[iRow][iCol] = fScalar*rkMatrix.m[iRow][iCol];
+
+Vector3 operator* (const Vector3& point, const Matrix3& matrix) {
+  Vector3 prod(0,0,0);
+  for (size_t row = 0; row < 3; row++) {
+    prod[row] =
+      point[0]*matrix.m[0][row] +
+      point[1]*matrix.m[1][row] +
+      point[2]*matrix.m[2][row];
   }
-  return kProd;
+  return prod;
 }
-
 //-----------------------------------------------------------------------
-Matrix3 Matrix3::Transpose () const
-{
-  Matrix3 kTranspose;
-  for (size_t iRow = 0; iRow < 3; iRow++)
-  {
-    for (size_t iCol = 0; iCol < 3; iCol++)
-      kTranspose[iRow][iCol] = m[iCol][iRow];
+
+Matrix3 Matrix3::operator* (Real scalar) const {
+  Matrix3 prod;
+  for (size_t row = 0; row < 3; row++) {
+    for (size_t col = 0; col < 3; col++)
+      prod[row][col] = scalar*m[row][col];
   }
-  return kTranspose;
+  return prod;
 }
-
 //-----------------------------------------------------------------------
-Matrix3 Matrix3::Inverse (Real fTolerance) const
-{
-  Matrix3 kInverse = Matrix3::ZERO;
-  Inverse(kInverse,fTolerance);
-  return kInverse;
-}
 
+Matrix3 operator* (Real scalar, const Matrix3& matrix) {
+  Matrix3 prod;
+  for (size_t row = 0; row < 3; row++) {
+    for (size_t col = 0; col < 3; col++)
+      prod[row][col] = scalar*matrix.m[row][col];
+  }
+  return prod;
+}
+//-----------------------------------------------------------------------
+
+Matrix3 Matrix3::Transpose () const {
+  Matrix3 transpose;
+  for (size_t row = 0; row < 3; row++) {
+    for (size_t col = 0; col < 3; col++)
+      transpose[row][col] = m[col][row];
+  }
+  return transpose;
+}
+//-----------------------------------------------------------------------
+
+Matrix3 Matrix3::Inverse (Real tolerance) const {
+  Matrix3 inverse = Matrix3::ZERO;
+  Inverse(inverse, tolerance);
+  return inverse;
+}
 //--------------------------------------------------------------------------
-bool Matrix3::Inverse (Matrix3& rkInverse, Real fTolerance) const
-{
+
+bool Matrix3::Inverse (Matrix3& inverse, Real tolerance) const {
   // Invert a 3x3 using cofactors.  This is about 8 times faster than
   // the Numerical Recipes code which uses Gaussian elimination.
-  rkInverse[0][0] = m[1][1]*m[2][2] -
+  inverse[0][0] = m[1][1]*m[2][2] -
     m[1][2]*m[2][1];
-  rkInverse[0][1] = m[0][2]*m[2][1] -
+  inverse[0][1] = m[0][2]*m[2][1] -
     m[0][1]*m[2][2];
-  rkInverse[0][2] = m[0][1]*m[1][2] -
+  inverse[0][2] = m[0][1]*m[1][2] -
     m[0][2]*m[1][1];
-  rkInverse[1][0] = m[1][2]*m[2][0] -
+  inverse[1][0] = m[1][2]*m[2][0] -
     m[1][0]*m[2][2];
-  rkInverse[1][1] = m[0][0]*m[2][2] -
+  inverse[1][1] = m[0][0]*m[2][2] -
     m[0][2]*m[2][0];
-  rkInverse[1][2] = m[0][2]*m[1][0] -
+  inverse[1][2] = m[0][2]*m[1][0] -
     m[0][0]*m[1][2];
-  rkInverse[2][0] = m[1][0]*m[2][1] -
+  inverse[2][0] = m[1][0]*m[2][1] -
     m[1][1]*m[2][0];
-  rkInverse[2][1] = m[0][1]*m[2][0] -
+  inverse[2][1] = m[0][1]*m[2][0] -
     m[0][0]*m[2][1];
-  rkInverse[2][2] = m[0][0]*m[1][1] -
+  inverse[2][2] = m[0][0]*m[1][1] -
     m[0][1]*m[1][0];
 
-  Real fDet =
-    m[0][0]*rkInverse[0][0] +
-    m[0][1]*rkInverse[1][0]+
-    m[0][2]*rkInverse[2][0];
+  Real det =
+    m[0][0]*inverse[0][0] +
+    m[0][1]*inverse[1][0]+
+    m[0][2]*inverse[2][0];
 
-  if ( abs(fDet) <= fTolerance )
+  if ( abs(det) <= tolerance )
     return false;
 
-  Real fInvDet = 1.0f/fDet;
-  for (size_t iRow = 0; iRow < 3; iRow++)
-  {
-    for (size_t iCol = 0; iCol < 3; iCol++)
-      rkInverse[iRow][iCol] *= fInvDet;
+  Real inv_det = 1.0f/det;
+  for (size_t row = 0; row < 3; row++) {
+    for (size_t col = 0; col < 3; col++)
+      inverse[row][col] *= inv_det;
   }
 
   return true;
--- src/gfx/math/matrix3.h
+++ src/gfx/math/matrix3.h
@@ -12,33 +12,29 @@ public:
 
   Matrix3 () {};
 
-  explicit Matrix3 (const float arr[3][3])
-  {
+  explicit Matrix3 (const float arr[3][3]) {
     memcpy(m,arr,9*sizeof(float));
   }
 
-  Matrix3 (const Matrix3& rkMatrix)
-  {
-    memcpy(m,rkMatrix.m,9*sizeof(float));
+  Matrix3 (const Matrix3& matrix) {
+    memcpy(m,matrix.m,9*sizeof(float));
   }
 
-  Matrix3 (Real fEntry00, Real fEntry01, Real fEntry02,
-    Real fEntry10, Real fEntry11, Real fEntry12,
-    Real fEntry20, Real fEntry21, Real fEntry22)
-  {
-    m[0][0] = fEntry00;
-    m[0][1] = fEntry01;
-    m[0][2] = fEntry02;
-    m[1][0] = fEntry10;
-    m[1][1] = fEntry11;
-    m[1][2] = fEntry12;
-    m[2][0] = fEntry20;
-    m[2][1] = fEntry21;
-    m[2][2] = fEntry22;
+  Matrix3 (Real entry00, Real entry01, Real entry02,
+    Real entry10, Real entry11, Real entry12,
+    Real entry20, Real entry21, Real entry22) {
+    m[0][0] = entry00;
+    m[0][1] = entry01;
+    m[0][2] = entry02;
+    m[1][0] = entry10;
+    m[1][1] = entry11;
+    m[1][2] = entry12;
+    m[2][0] = entry20;
+    m[2][1] = entry21;
+    m[2][2] = entry22;
   };
 
-  void swap(Matrix3& other)
-  {
+  void swap(Matrix3& other) {
     std::swap(m[0][0], other.m[0][0]);
     std::swap(m[0][1], other.m[0][1]);
     std::swap(m[0][2], other.m[0][2]);
@@ -50,26 +46,25 @@ public:
     std::swap(m[2][2], other.m[2][2]);
   }
 
-  float* operator[] (size_t row) const
-  {
+  float* operator[] (size_t row) const {
     return (float*)m[row];
   }
 
-  void SetColumn(size_t iCol, const Vector3& vec);
+  void SetColumn(size_t col, const Vector3& vec);
 
-  Vector3 operator* (const Vector3& rkVector) const;
+  Vector3 operator* (const Vector3& vector) const;
 
-  friend Vector3 operator* (const Vector3& rkVector,
-    const Matrix3& rkMatrix);
+  friend Vector3 operator* (const Vector3& vector,
+    const Matrix3& matrix);
 
-  Matrix3 operator* (Real fScalar) const;
+  Matrix3 operator* (Real scalar) const;
 
-  friend Matrix3 operator* (Real fScalar, const Matrix3& rkMatrix);
+  friend Matrix3 operator* (Real scalar, const Matrix3& matrix);
 
   void FromAxes(const Vector3& xAxis, const Vector3& yAxis, const Vector3& zAxis);
 
-  bool Inverse (Matrix3& rkInverse, Real fTolerance = 1e-06) const;
-  Matrix3 Inverse (Real fTolerance = 1e-06) const;
+  bool Inverse (Matrix3& inverse, Real tolerance = 1e-06) const;
+  Matrix3 Inverse (Real tolerance = 1e-06) const;
 
   Matrix3 Transpose() const;
 
