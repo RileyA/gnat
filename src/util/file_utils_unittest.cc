b170787b8ea862fb28636edd28f20ea3d468c75c
Riley Adams <rileya@cs.washington.edu>
2014-04-22 18:37:17 -0700

    Add file util test file.

--- src/util/file_utils_unittest.cc
+++ src/util/file_utils_unittest.cc
@@ -6,7 +6,7 @@ namespace gnat {
 TEST(FileUtilsTest, ReadFile) {
   String out;
   EXPECT_TRUE(FileUtils::ReadFile(FileUtils::GetBaseFilePath() +
-                                  "data/test/file.txt", &out));
+                                  "../data/test/file.txt", &out));
   EXPECT_EQ(String("Line 1\nLine 2\n"), out);
 }
 
