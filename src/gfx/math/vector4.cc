0604e2520e47bd853bf390b4e6a76d34f1e24548
Riley Adams <rileya@cs.washington.edu>
2014-07-16 15:08:13 -0700

    Get very simple SSE function working for Vector4 addition.

--- src/gfx/math/vector4.cc
+++ src/gfx/math/vector4.cc
@@ -13,4 +13,12 @@ const Vector4 Vector4::NEGATIVE_UNIT_Y(  0.0, -1.0,  0.0, 1.0);
 const Vector4 Vector4::NEGATIVE_UNIT_Z(  0.0,  0.0, -1.0, 1.0);
 const Vector4 Vector4::UNIT_SCALE(1.0, 1.0, 1.0, 1.0);
 
+#ifdef USE_VECTOR4_SIMD
+Vector4 Vector4::operator +(const Vector4& v) const {
+  Vector4 out = v;
+  vector4_add_simd(&x, &out.x);
+  return out;
+}
+#endif
+
 }
