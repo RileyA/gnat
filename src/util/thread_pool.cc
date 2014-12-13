2106eb5a03c8b4a49e2b77cd655fe282984dc5cb
Riley Adams <rileya@cs.washington.edu>
2014-12-12 20:13:24 -0800

    Tabs -> spaces in util/thread_pool.*

--- src/util/thread_pool.cc
+++ src/util/thread_pool.cc
@@ -3,23 +3,23 @@
 namespace gnat {
 
 ThreadPool::ThreadPool(int num_threads)
-	: num_threads_(num_threads) {
-	for(int i = 0; i < num_threads; ++i)
-		thread_pool_.push_back(new std::thread(&WorkerThread, this));
-	active_jobs_ = 0;
-	pending_jobs_ = 0;
+  : num_threads_(num_threads) {
+  for(int i = 0; i < num_threads; ++i)
+    thread_pool_.push_back(new std::thread(&WorkerThread, this));
+  active_jobs_ = 0;
+  pending_jobs_ = 0;
 }
 //---------------------------------------------------------------------------
 
 ThreadPool::~ThreadPool() {
-	// flood the job queue with null jobs...
-	for(int i = 0; i < num_threads_ * 2; ++i)
-		AddJob(0);
+  // flood the job queue with null jobs...
+  for(int i = 0; i < num_threads_ * 2; ++i)
+    AddJob(0);
 
-	// start the workers (no workers should be working at this point)
-	StartWorkers();
+  // start the workers (no workers should be working at this point)
+  StartWorkers();
 
-	//  let them all wrap up
+  //  let them all wrap up
   for (int i = 0; i < thread_pool_.size(); ++i) {
     thread_pool_[i]->join();
     delete thread_pool_[i];
@@ -28,78 +28,78 @@ ThreadPool::~ThreadPool() {
 //---------------------------------------------------------------------------
 
 void ThreadPool::AddJob(Job* job) {
-	std::lock_guard<std::mutex> lock(job_mutex_);
-	jobs_.push_back(job);
-	++pending_jobs_;
+  std::lock_guard<std::mutex> lock(job_mutex_);
+  jobs_.push_back(job);
+  ++pending_jobs_;
 }
 //---------------------------------------------------------------------------
 
 void ThreadPool::StartWorkers() {
-	std::lock_guard<std::mutex> lock(job_mutex_);
-	// just notify one, it'll notify more if need be
-	if(!jobs_.empty())
-		job_signal_.notify_one();
+  std::lock_guard<std::mutex> lock(job_mutex_);
+  // just notify one, it'll notify more if need be
+  if(!jobs_.empty())
+    job_signal_.notify_one();
 }
 //---------------------------------------------------------------------------
 
 void ThreadPool::WaitForWorkers() {
-	// wait for all current jobs to complete
-	std::lock_guard<std::mutex> lock(job_mutex_);
-	while(pending_jobs_ > 0)
-		job_done_signal_.wait(job_mutex_);
+  // wait for all current jobs to complete
+  std::lock_guard<std::mutex> lock(job_mutex_);
+  while(pending_jobs_ > 0)
+    job_done_signal_.wait(job_mutex_);
 }
 //---------------------------------------------------------------------------
 
 void ThreadPool::WorkerThread(ThreadPool* pool) {
-	Job* assigned = 0;
-	bool more_jobs = false;
-	bool has_job = false; // poor unemployed worker thread.. ;_;
+  Job* assigned = 0;
+  bool more_jobs = false;
+  bool has_job = false; // poor unemployed worker thread.. ;_;
 
-	// the only way it will terminate is if it receives a null job
-	while(true) {
-		// lock to access job queue
-		{
+  // the only way it will terminate is if it receives a null job
+  while(true) {
+    // lock to access job queue
+    {
       std::lock_guard<std::mutex> lock(pool->job_mutex_);
 
-			// skip if this is the first iteration
-			if(has_job) {
-				// must've just wrapped up a job
-				--pool->active_jobs_;
-				--pool->pending_jobs_;
+      // skip if this is the first iteration
+      if(has_job) {
+        // must've just wrapped up a job
+        --pool->active_jobs_;
+        --pool->pending_jobs_;
 
-				// notify if there are no jobs left
-				if(pool->pending_jobs_ == 0)
-					pool->job_done_signal_.notify_one();
-	
-				// if the last job was null, then terminate
-				if(!assigned)
-					return;
-				else
-					delete assigned;
-			}
+        // notify if there are no jobs left
+        if(pool->pending_jobs_ == 0)
+          pool->job_done_signal_.notify_one();
+  
+        // if the last job was null, then terminate
+        if(!assigned)
+          return;
+        else
+          delete assigned;
+      }
 
-			// loop on the condition, to handle spurious wakeups
-			// note that this will get skipped if jobs remain in the queue after being awoken
-			while(pool->jobs_.empty())
-				pool->job_signal_.wait(pool->job_mutex_);
+      // loop on the condition, to handle spurious wakeups
+      // note that this will get skipped if jobs remain in the queue after being awoken
+      while(pool->jobs_.empty())
+        pool->job_signal_.wait(pool->job_mutex_);
 
-			// if it gets here, there must be a job for it...
-			assigned = pool->jobs_.front();
-			pool->jobs_.pop_front();
-			has_job = true;
-			++pool->active_jobs_;
+      // if it gets here, there must be a job for it...
+      assigned = pool->jobs_.front();
+      pool->jobs_.pop_front();
+      has_job = true;
+      ++pool->active_jobs_;
 
-			more_jobs = !pool->jobs_.empty();
-		}
+      more_jobs = !pool->jobs_.empty();
+    }
 
-		// now that the mutex is unlocked, if there's still work to be done, notify another thread
-		if(more_jobs)
-			pool->job_signal_.notify_one();
+    // now that the mutex is unlocked, if there's still work to be done, notify another thread
+    if(more_jobs)
+      pool->job_signal_.notify_one();
 
-		// actually do the work...
-		if(assigned)
-			assigned->Execute();
-	}
+    // actually do the work...
+    if(assigned)
+      assigned->Execute();
+  }
 }
 //---------------------------------------------------------------------------
 
--- src/util/thread_pool.h
+++ src/util/thread_pool.h
@@ -12,57 +12,57 @@
 namespace gnat {
 
 /** A simple and very limited interface that allows you to make a pool of threads
- *	do work from a queue of jobs */
+ *  do work from a queue of jobs */
 class ThreadPool {
  public:
-	/** Constructor
-	 *	@param numThreads How many threads in the pool */
-	ThreadPool(int num_threads);
-	~ThreadPool();
+  /** Constructor
+   *  @param numThreads How many threads in the pool */
+  ThreadPool(int num_threads);
+  ~ThreadPool();
 
-	/** A single unit of work for a thread to do */
-	class Job {
-	 public:
-		virtual void Execute() = 0;
-	};
+  /** A single unit of work for a thread to do */
+  class Job {
+   public:
+    virtual void Execute() = 0;
+  };
 
-	/** Add a job to the queue
-	 *		@param job The job
-	 *		@remarks Adding a NULL job will cause the worker that gets
-	 *			that job to terminate */
-	void AddJob(Job* job);
+  /** Add a job to the queue
+   *    @param job The job
+   *    @remarks Adding a NULL job will cause the worker that gets
+   *      that job to terminate */
+  void AddJob(Job* job);
 
-	/** Start the workers working... */
-	void StartWorkers();
+  /** Start the workers working... */
+  void StartWorkers();
 
-	/** Wait until all of the workers are done */
-	void WaitForWorkers();
+  /** Wait until all of the workers are done */
+  void WaitForWorkers();
 
-	/** Function used by worker threads */
-	static void WorkerThread(ThreadPool* pool);
+  /** Function used by worker threads */
+  static void WorkerThread(ThreadPool* pool);
 
  protected:
 
-	// mutex that protects the job queue
-	std::mutex job_mutex_;
+  // mutex that protects the job queue
+  std::mutex job_mutex_;
 
-	// the queue of pending jobs
-	std::deque<Job*> jobs_;
+  // the queue of pending jobs
+  std::deque<Job*> jobs_;
 
-	// counters of pending and active jobs
-	unsigned int active_jobs_;
-	unsigned int pending_jobs_;
+  // counters of pending and active jobs
+  unsigned int active_jobs_;
+  unsigned int pending_jobs_;
 
-	// how many threads in the pool (fixed atm, may be variable later...)
-	unsigned int num_threads_;
+  // how many threads in the pool (fixed atm, may be variable later...)
+  unsigned int num_threads_;
 
-	// used to signal workers that there's work to be done
-	std::condition_variable_any job_signal_;
+  // used to signal workers that there's work to be done
+  std::condition_variable_any job_signal_;
 
-	// used to signal that the jobs are done
+  // used to signal that the jobs are done
   std::condition_variable_any job_done_signal_;
 
-	// the pool of threads itself
+  // the pool of threads itself
   std::vector<std::thread*> thread_pool_;
 };
 
